20cd6e488d979151116f7e5d34865c0ded20dd71
Kate Sohng <kheeya7@hotmail.com>
2018-02-19 19:55:07 -0800

    Deleting all the references that were needed by the Azure Mobile service.

--- unify_app/JobPostingListViewController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface JobPostingListViewController : UITableViewController<NSFetchedResultsControllerDelegate, UITableViewDataSource>
-
-@property (weak, nonatomic) IBOutlet UITextField *itemText;
-- (IBAction)onAdd:(id)sender;
-
-@end
