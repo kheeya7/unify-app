21fa4728223020a034452928758152dd1b3dc0c2
Kate Sohng <kheeya7@hotmail.com>
2018-02-19 22:59:13 -0800

    created a user model. after sign in, we save the user to the appDelegate property, and show that user information in the dashboard page.

---
+++ unify_app/DashboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  DashboardViewController.h
+//  unify_app
+//
+//  Created by Kate Sohng on 2/19/18.
+//  Copyright © 2018 Unify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashboardViewController : UIViewController
+
+@end
---
+++ unify_app/User.m
@@ -0,0 +1,27 @@
+//
+//  User.m
+//  unify_app
+//
+//  Created by Kate Sohng on 2/19/18.
+//  Copyright © 2018 Unify. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+- (id) initWithId: (NSString *)aUid
+      displayName: (NSString *)aDisplayName
+            email: (NSString *)aEmail
+         photoUrl: (NSURL *)aPhotoUrl {
+    if (self = [super init]) {
+        self.uid = aUid;
+        self.displayName = aDisplayName;
+        self.email = aEmail;
+        self.photoUrl = aPhotoUrl;
+    }
+    
+    return self;
+}
+
+@end
