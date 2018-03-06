6430b34ddb16997a4615ace164249b30043854ce
Kate Sohng <kheeya7@hotmail.com>
2018-03-05 18:54:51 -0800

    Changing the background image, the order of the tab bat controller items by creating a new tab bar controller so that the first selected scene is the home, removing some animation

---
+++ unify_app/MainTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainTabBarViewController.h
+//  unify_app
+//
+//  Created by Kate Sohng on 3/5/18.
+//  Copyright © 2018 Unify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTabBarViewController : UITabBarController
+
+@end
