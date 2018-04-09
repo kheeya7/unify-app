df06cae3bcc42d97ef786b238c2c74c47f3082d6
rcaligan <rcaligan@d-10-156-25-64.dhcp4.washington.edu>
2018-04-09 14:02:44 -0700

    Added view controller screens for pullout menu, groups/group detail, members, about, saved jobs, contact us

---
+++ unify_app/ContactUsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactUsViewController.h
+//  unify_app
+//
+//  Created by rcaligan on 4/9/18.
+//  Copyright © 2018 Unify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactUsViewController : UIViewController
+
+@end
---
+++ unify_app/SavedJobsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SavedJobsViewController.h
+//  unify_app
+//
+//  Created by rcaligan on 4/9/18.
+//  Copyright © 2018 Unify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SavedJobsViewController : UIViewController
+
+@end
