4538038d4874b323a93cbdc5e630679f306d1f01
savannahcatalyst <savannahcatalyst@msn.com>
2018-04-18 15:04:51 -0700

    Added jobs and company controller with toggle view

---
+++ unify_app/JobsAndCompaniesViewController.h
@@ -0,0 +1,13 @@
+//
+//  JobsAndCompaniesViewController.h
+//  unify_app
+//
+//  Created by Savannah Schuchardt on 4/18/18.
+//  Copyright © 2018 Unify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JobsAndCompaniesViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
