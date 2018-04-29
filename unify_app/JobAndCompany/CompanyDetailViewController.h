4d15c48bf85bdf2e9a0761f0f75187af0d496377
Kate Sohng <kheeya7@hotmail.com>
2018-04-28 20:09:57 -0700

    Created Company Detail View Controller, and pass the data into the scene

---
+++ unify_app/JobAndCompany/CompanyDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  CompanyDetailViewController.h
+//  unify_app
+//
+//  Created by Kate Sohng on 4/28/18.
+//  Copyright © 2018 Unify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Company.h"
+
+@interface CompanyDetailViewController : UIViewController
+
+@property (nonatomic) Company *currentCompany;
+
+@end
